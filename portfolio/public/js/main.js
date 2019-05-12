d578d866db37824c6ca113a35e7be55dcb06317f
Miguel <miguelsolans@icloud.com>
2019-05-13 00:41:06 +0100

    Init
    Init

---
+++ portfolio/public/js/main.js
@@ -0,0 +1,237 @@
+requirejs([
+    'jquery',
+    'instafeed',
+    'facebook',
+    'setlistfm'
+], function($, Instafeed,) {
+
+    function getLink(txt) {
+        var string = txt.match(/\bhttps?:\/\/\S+/gi);
+
+        return string ? string : undefined;
+    }
+
+    // Facebook API
+    /**
+     *
+     */
+    FB.init({
+        appId      : '1895870987181544',
+        version    : 'v3.3'
+    });
+    /**
+     *
+     */
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+
+            /**
+             * Get Page profile Picture
+             */
+            FB.api('/325105178191793?fields=picture.height(900)', function(data) {
+                var imgTag = "<img class='portrait' src='" + data.picture.data.url + "'>";
+
+                document.getElementById('profile-picture').innerHTML += imgTag;
+            });
+
+
+            /**
+             * Get Information about Page
+             */
+            FB.api('/325105178191793?fields=about', function(data) {
+                //console.log(data);
+                document.getElementById('description').innerHTML = data.about;
+            });
+
+            /**
+             * Get certificates images
+             */
+            // 331562370879407?fields=picture
+            // 331562370879407?fields=photos{name,images}
+            // As maiores imagens vêm sempre em images[0].source
+            FB.api('331562370879407?fields=photos{picture,name}', function(data) {
+                //console.dir(data);
+                var photos = data.photos.data;
+                for(var i in photos) {
+                    var img = "<img src=" + photos[i].picture + ">";
+
+
+                    document.getElementById('certificates-img').innerHTML += img;
+
+                    var url;
+                    var string = getLink(photos[i].name);
+
+                    if(string) {
+                        url = "<br><a href='" + string[0] + "'>Link</a>";
+                        document.getElementById('certificates-img').innerHTML += url;
+                    }
+                }
+
+                //var photo = "<img src=" + data.picture.data.url + ">";
+                //document.getElementById('certificates-img').innerHTML += photo;
+
+            });
+
+            FB.api('332124090823235?fields=photos{picture,name}', function(data) {
+                // console.log(data);
+
+                var photos = data.photos.data;
+
+                for(var i in photos) {
+                    var img = "<img src='" + photos[i].picture + "'>";
+
+                    document.getElementById('projects-img').innerHTML += img;
+
+                    var url;
+                    var string = getLink(photos[i].name);
+
+                    if(string) {
+                        url = "<br><a href='" + string[0] + "'>Link</a>";
+                        document.getElementById('projects-img').innerHTML += url;
+                    }
+
+                }
+
+            });
+
+            // FB.api('332124090823235?fields=picture', function (data) {
+            //     var photo = "<img src=" + data.picture.data.url + ">";
+            //
+            //     document.getElementById('projects-img').innerHTML += photo;
+            // });
+            /**
+             * Get page name
+             */
+            FB.api("/325105178191793?fields=name", function(data) {
+                document.getElementById('name').innerText = data.name;
+            });
+
+            // FB.ui({
+            //     method: 'share',
+            //     href: 'https://developers.facebook.com/docs/',
+            //     quote: 'Hello World!'
+            // }, function(response){
+            //     console.log("Share Response:" + response);
+            // });
+
+
+            // FB.api(`/325105178191793/photos`, 'post', params, (response) => {
+            //     if (response && response.error) {
+            //         //upload failed
+            //     } else {
+            //         //upload successful
+            //     }
+            // });
+
+
+        } else if (response.status === 'not_authorized') {
+            // The user hasn't authorized your application.  They
+            // must click the Login button, or you must call FB.login
+            // in response to a user gesture, to launch a login dialog.
+            console.log("Not Authorized!");
+        } else {
+            // The user isn't logged in to Facebook. You can launch a
+            // login dialog with a user gesture, but the user may have
+            // to log in to Facebook before authorizing your application.
+            console.log("Not logged in!");
+        }
+    });
+
+
+    // Get LastFM album artworks
+    // Link: http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=miguelsolans&api_key=b9704a63e108fe1e52d55c0226ded89e&format=json
+    $.getJSON('https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=miguelsolans&api_key=b9704a63e108fe1e52d55c0226ded89e&format=json', function(data) {
+        console.log(data);
+
+        for(let i = 0; i < 6; i++) {
+            var imgTag = "<img src=" + data.topalbums.album[i].image[3]["#text"] + ">";
+            var artistInfo = "<p>" + data.topalbums.album[i].artist.name +"</p>";
+            var playCount = "<p>" + data.topalbums.album[i].playcount + "</p>";
+            document.getElementById('music').innerHTML += imgTag;
+            // document.getElementById('music').innerHTML += artistInfo;
+        }
+    });
+
+    // $.getJSON('https://ws.audioscrobbler.com/2.0/?method=user.getweeklyalbumchart&user=miguelsolans&api_key=b9704a63e108fe1e52d55c0226ded89e&format=json', function(data) {
+    //     console.log(data);
+    //
+    //     for(let i = 0; i < 4; i++) {
+    //         var imgTag = "<img src=" + data.weeklyalbumchart.album[i].image[3]["#text"] + ">";
+    //         var artistInfo = "<p>" + data.weeklyalbumchart.album[i].artist.name +"</p>";
+    //         var playCount = "<p>" + data.weeklyalbumchart.album[i].playcount + "</p>";
+    //         document.getElementById('music').innerHTML += imgTag;
+    //         document.getElementById('music').innerHTML += artistInfo;
+    //     }
+    // });
+
+    // $.getJSON('https://ws.audioscrobbler.com/2.0/?method=user.gettoptags&user=miguelsolans&api_key=b9704a63e108fe1e52d55c0226ded89e&format=json', function(data) {
+    //     console.log("Musical Interests");
+    //     console.log(data);
+    // });
+
+
+    // Instagram feed
+    var userFeed = new Instafeed({
+        get: 'user',
+        // miguelsolans: 356670749
+        //              Token: 356670749.1677ed0.7401560dd16d424a9744967925c234db
+        // metalstigma: 7516438355
+        //              Token: 7516438355.1677ed0.6dc1f61c766f44ad8a85fac1abff991d
+        // worldby_lenses: 13531427925
+        //              Token: 13531427925.1677ed0.8079190ddfb4467789a1586f50c89e17
+        userId: '356670749',
+        accessToken: '356670749.1677ed0.7401560dd16d424a9744967925c234db',
+        	//filter: function(image) {
+        	//	return image.tags.indexOf('metal') >= 0;
+        	//},
+        limit: 10,
+        template: '<a href="{{link}}" target="_blank"><img class="col-md-10" src="{{image}}" /></a>',
+        // template: '<a href="{{link}}" target="_blank"><img class="insta-pic" src="{{image}}" /></a>',
+        resolution: 'standard_resolution'
+    });
+    userFeed.run();
+
+
+
+
+
+    // Setlist fm config
+    // $.ajax({
+    //     url: "https://api.setlist.fm/rest/1.0/user/miguelsolans/attended?p=1",
+    //     method: "GET",
+    //     headers: {
+    //         "Authorization": "4b54b070-8e29-416c-bf35-ff6b8f6e7eeb",
+    //         'Content-Type': 'application/json'
+    //     },
+    // }).then(function(response) {
+    //     console.log(response);
+    // }).catch(function(err) {
+    //     console.error(err);
+    // });
+    // $.ajax({
+    //     method: "GET",
+    //     url: "https://api.setlist.fm/rest/1.0/user/miguelsolans/attended?p=1",
+    //     headers: {
+    //         "x-api-key": "4b54b070-8e29-416c-bf35-ff6b8f6e7eeb",
+    //         "Accept": 'application/json'
+    //     },
+    //     success: function(data) {
+    //         console.log(data);
+    //     },
+    //     dataType: "json"
+    // });
+
+
+    /*
+    method: 'GET',
+        withCredentials: true,
+        credentials: 'include',
+        headers: {
+            'Authorization': bearer,
+            'X-FP-API-KEY': 'iphone', //it can be iPhone or your any other attribute
+            'Content-Type': 'application/json'
+        }
+        //Accept: application/json
+     */
+
+});
---
+++ portfolio/server.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const bodyParser= require('body-parser'); // to handle form posts
+const app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+
+// ejs
+app.set('view engine', 'ejs');
+
+// urlencoded tells body-parser to extract data from <from>
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(3000, function() {
+    console.log("Magic Port: 3000");
+});
+
+
+const MongoClient = require('mongodb').MongoClient;
+var db;
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    // ... start the server
+    // If there was an error establishing connection to mongodb
+    if(err) return console.log(err);
+
+    // get DB name
+    db = client.db('portfolio');
+    // console.log(db);
+
+});
+
+// req : request
+// res : response
+// app.get('/', function(req, res) {
+//     res.send("Hello World!");
+// });
+
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + "/index.html");
+// });
+
+//The 404 Route (ALWAYS Keep this as the last route)
+// app.get('*', function(req, res){
+//     res.render('404.ejs');
+// });
+
+app.get('/', (req, res) => {
+    var education = {};
+    var jobs = {};
+    db.collection('education').find().toArray((err, result) => {
+        if (err) console.log(err);
+
+        db.collection('jobs').find().toArray((err, jobsResult) => {
+            if (err) console.log(err);
+
+            res.render('index.ejs', {
+                education: result,
+                job: jobsResult
+            })
+        });
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    // SELECT * FROM users WHERE username = something and password = somethingElse
+    db.collection('users').find({username: req.body.username, password: req.body.password}).toArray(function(err, result) {
+        if(result.length !== 0) {
+            res.render('admin.ejs');
+        }
+        else {
+            res.send("<h2>Couldn't Login</h2>");
+        }
+    })
+});
+
+// Create Operatons
+app.post('/newEducation', (req, res) => {
+    var education = {
+        school: req.body.school,
+        course: req.body.course
+    };
+    db.collection('education').insertOne(education, function(err, res) {
+        if (err) throw err;
+        console.log("New Education Added");
+    });
+});
+app.post('/newJob', (req, res) => {
+    console.log(req.body);
+
+    var job = {
+        company: req.body.company,
+        description: req.body.description
+    };
+
+    job.flag = req.body.flag === 'true' ? true : false;
+
+    db.collection('jobs').insertOne(job, function(err, res) {
+        if (err) throw err;
+        alert("New Job Added")
+    });
+    res.render('login.ejs')
+});
+
+
+app.get('/test', function(req, res) {
+    db.collection('education').find().toArray(function(err, result) {
+        console.log(result);
+    });
+});
